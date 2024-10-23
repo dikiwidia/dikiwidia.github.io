af5a5c44120ae0d2c4ac79ebf66f0f4eb6806647
Moch Diki Widianto <dikiwidia@live.com>
2024-10-23 20:59:58 +0700

    update info

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -12,9 +12,10 @@ import {
   FaRegStar,
   FaStar,
   FaStarHalfAlt,
+  FaVuejs,
 } from "react-icons/fa";
-import { FaGithub, FaStarHalf, FaWhatsapp } from "react-icons/fa6";
-import { GiElephant } from "react-icons/gi";
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa6";
+import mochdikiwidianto from "../assets/mochdikiwidianto.png";
 
 const MainPage = () => {
   return (
@@ -26,59 +27,70 @@ const MainPage = () => {
           alt="Picture"
           className="w-full h-48 object-cover rounded-t-lg"
         />
-        <div className="flex justify-between items-center my-8 mx-8 relative">
+        <div className="flex justify-between items-start my-8 mx-8 relative">
           <div className="absolute left-0 -top-32">
             <img
-              src="https://picsum.photos/300/300"
+              src={mochdikiwidianto}
               alt=""
-              className="rounded-full w-48 h-48 object-cover"
+              className="rounded-full w-48 h-48 object-cover bg-slate-300"
             />
           </div>
-          <div className="flex flex-col items-start ml-56">
-            <h1 className="text-2xl font-bold">Moch Diki Widianto</h1>
+          <div className="flex flex-col items-start ml-56 pr-36">
+            <h1 className="text-2xl font-bold mb-2">Moch Diki Widianto</h1>
             <p className="text-md">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Voluptatum, enim!
+              Halo semua, Selamat datang di situsweb portofolio sederhana ini,
+              Saya adalah seorang <i>fullstack developer</i> dari Rangkasbitung,
+              Banten, Indonesia 🇮🇩
             </p>
           </div>
           <div className="flex justify-end items-center gap-1">
             <a
-              href="#"
+              href="https://wa.me/6289735700005"
+              target="_blank"
               className="flex justify-start items-center border border-green-600 text-green-600 shadow-md hover:bg-green-600 hover:text-white font-bold py-2 px-4 rounded"
             >
               <FaWhatsapp className="mr-2 w-6 h-6" />
               Whatsapp
             </a>
             <a
-              href="#"
-              className="flex justify-start items-center border border-slate-700 text-slate-700 shadow-md hover:bg-slate-700 hover:text-white font-bold py-2 px-4 rounded"
+              href="https://www.linkedin.com/in/mochdikiwidianto/"
+              target="_blank"
+              className="flex justify-start items-center border border-blue-700 text-blue-700 shadow-md hover:bg-blue-700 hover:text-white font-bold py-2 px-4 rounded"
             >
-              <FaGithub className="mr-2 w-6 h-6" />
-              Github
+              <FaLinkedin className="mr-2 w-6 h-6" />
+              LinkedIn
             </a>
           </div>
         </div>
         {/* Profile */}
-        <div className="flex justify-between items-start mx-8 mt-14 mb-8">
-          <div className="flex flex-col justify-start w-2/3 pr-8">
-            <h1 className="text-lg font-bold mb-2">Tentang Saya</h1>
+        <div className="flex justify-between items-start mx-8 mt-14 mb-16">
+          <div className="flex flex-col justify-start w-2/3 pr-16">
+            <h1 className="text-lg font-bold mb-4">Tentang Saya</h1>
+            <p className="text-md mb-4">
+              Perkenalkan Saya Moch Diki Widianto dari Rangkasbitung, Banten,
+              Indonesia. Saya merupakan seorang <i>fullstack developer</i> yang
+              mempelajari bahasa pemrograman secara otodidak mencari informasi
+              dari berbagai sumber di internet. Saya menyukai dunia pemrograman
+              dan familiar dengan beberapa bahasa pemrogaram dan berbagai macam{" "}
+              <i>framework</i>.
+            </p>
             <p className="text-md">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-              voluptatem, quasi similique, incidunt, voluptatum excepturi
-              veritatis error quidem officiis voluptates tempora. Dignissimos
-              quos consequatur, dolorum, deleniti accusamus autem exercitationem
-              doloremque.
+              Beberapa diantaranya PHP, Laravel, React, VueJs, dan masih banyak
+              lagi. Untuk dapat terhubung dengan Saya anda dapat menghubungi
+              Saya melalui Whatsapp, Email, atau LinkedIn yang tertera pada
+              halaman ini.
             </p>
           </div>
-          <div className="flex justify-start items-start flex-wrap w-1/3 p">
+          <div className="flex justify-start items-start flex-wrap w-1/3">
             <div className="flex flex-col w-1/2 px-4 my-4">
               <h1 className="font-bold text-slate-600">Wilayah Negara</h1>
-              <p className="text-md">Banten, Indonesia (ID)</p>
+              <p className="text-md">Rangkasbitung, Banten, Indonesia 🇮🇩</p>
             </div>
             <div className="flex flex-col w-1/2 px-4 my-4">
               <h1 className="font-bold text-slate-600">Situs Web</h1>
               <a
-                href="#"
+                href="https://dikiwidia.github.io"
+                target="_blank"
                 className="flex items-center text-md text-emerald-700 hover:text-emerald-800"
               >
                 dikiwidia.github.com
@@ -89,6 +101,7 @@ const MainPage = () => {
               <h1 className="font-bold text-slate-600">Surat Elektronik</h1>
               <a
                 href="mailto:diki.widianto@unilam.ac.id"
+                target="_blank"
                 className="flex items-center text-md text-emerald-700 hover:text-emerald-800"
               >
                 diki.widianto@unilam.ac.id
@@ -96,9 +109,10 @@ const MainPage = () => {
               </a>
             </div>
             <div className="flex flex-col w-1/2 px-4 my-4">
-              <h1 className="font-bold text-slate-600">LinkedIn</h1>
+              <h1 className="font-bold text-slate-600">GitHub</h1>
               <a
-                href="#"
+                href="https://github.com/dikiwidia"
+                target="_blank"
                 className="flex items-center text-md text-emerald-700 hover:text-emerald-800"
               >
                 Moch Diki Widianto
@@ -222,6 +236,22 @@ const MainPage = () => {
                   <FaStar className="w-4 h-4 text-orange-600" />
                   {/* <FaStarHalfAlt className="w-4 h-4 text-orange-600" />
                   <FaRegStar className="w-4 h-4 text-orange-600" /> */}
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-start items-center border border-emerald-300 rounded py-2 px-4">
+              <FaVuejs className="w-16 h-16 text-green-500" />
+              <div className="flex flex-col justify-center items-start ml-2">
+                <p className="font-bold">VueJs</p>
+                <p className="text-sm mb-1 italic">VueJs Framework</p>
+                <p className="flex text-sm gap-1">
+                  <FaStar className="w-4 h-4 text-orange-600" />
+                  <FaStar className="w-4 h-4 text-orange-600" />
+                  <FaStar className="w-4 h-4 text-orange-600" />
+                  <FaStar className="w-4 h-4 text-orange-600" />
+                  {/* <FaStar className="w-4 h-4 text-orange-600" /> */}
+                  <FaStarHalfAlt className="w-4 h-4 text-orange-600" />
+                  {/* <FaRegStar className="w-4 h-4 text-orange-600" /> */}
                 </p>
               </div>
             </div>
